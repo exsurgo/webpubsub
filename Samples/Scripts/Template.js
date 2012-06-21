00620aafcfcb611c18001d35dfe7aea9821f4e04
exsurgo <exsurgo@gmail.com>
2012-06-20 22:23:18 -0400

    Refactoring template code.

--- Samples/Scripts/Template.js
+++ Samples/Scripts/Template.js
@@ -1,20 +1,27 @@
 ﻿
+//Open connection
 PubSub.open(function () {
 
+    //Create a channel, Can be any string
     var channel = "my-channel";
-
+    
+    //Subscribe to the channel
     this.subscribe(channel, function (data) {
-
+        //...   
     })
-    .query(channel, function (users) {
-
+    //Query other users current on the channel
+    .query(channel, function (otherUsers) {
+        //...
     })
+    //Another user has connected to the channel
     .connect(channel, function (user) {
-
+        //...
     })
+    //Another user has disconnected from the channel
     .disconnect(channel, function (user) {
-
+        //...
     })
+    //Publish a message to the channel
     .publish(channel, { data: "Hello World" });
 
 });
